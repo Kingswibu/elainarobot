769054e5aa6842c743d2992c465687c11560b137
Kingswibu <99636581+Kingswibu@users.noreply.github.com>
2022-02-22 14:53:34 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -15,10 +15,10 @@ PHOTO = "https://telegra.ph/file/2c7c264b0648e8baee99e.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Elaina Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [iki](https://t.me/kingswibu)** \n\n"
+  TEXT += f"⚪ **My Master : [iki](https://t.me/skytrixsz)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Elaina_k_robot?start=help"), Button.url("listbot", "https://t.me/ikilistbot")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Elainaqrobot?start=help"), Button.url("listbot", "https://t.me/ikilistbot")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
