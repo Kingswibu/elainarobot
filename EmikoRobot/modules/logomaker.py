ed677f29f6510b573a66111c644c3740be9f4daf
Kingswibu <99636581+Kingswibu@users.noreply.github.com>
2022-02-23 22:11:28 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [Elaina Robot](https://t.me/elainaqrobot)",
             force_document=False,
         )
         os.remove(flnme)
